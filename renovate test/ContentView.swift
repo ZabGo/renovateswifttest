723fd47b49587b7119cc083199575e33d8979bb0
Xavier Godard <xavier.godard@xdesign.com>
2023-11-24 12:13:14 +0000

    Initial Commit

---
+++ renovate test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  renovate test
+//
+//  Created by Xavier Godard on 24/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ renovate test/renovate_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  renovate_testApp.swift
+//  renovate test
+//
+//  Created by Xavier Godard on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct renovate_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
